023ef56bbfa26f5a20c5fa85a155c549a655b89d
ruhan09 <ruhan.mohd@yahoo.com>
2019-01-03 07:59:06 -0500

    update code for multiple container creation

--- SetUpWorkstationConainer.sh
+++ SetUpWorkstationConainer.sh
@@ -2,37 +2,42 @@
 
 #Constant
 Image=bigdatalabs/workstation
-#ContainerIp=10.0.7.49
-#SshPort=35353
 ContainerRestartPolicy=always
 SwarmManagerIp=54.39.16.42
 Network=ibd
 DatasetsVol=/root/datasets
 UserHome=$1
-ContainerIp=$2
+ContainerName=$2
 SshPort=$3
-ip=172.26.201.1
-# Remove Docker if exists
-sudo apt-get purge -y docker-ce;sudo rm -rf /var/lib/docker
+ip=10.196.0.1
 
-#Install docker-ce=18.06.0~ce~3-0~ubuntu
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+if [ "$#" -ne 3 ]; then
+	echo "Incorrect number of parameters"
+	echo "Command: ./SetUpWorkstationConainer.sh <UserHomeLocation> <ContainerName> <SshPort>"
+	exit 0
+fi
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt-get update
-
-sudo apt-get install -y docker-ce=18.06.0~ce~3-0~ubuntu
+docker version &> /dev/null
+if [ $? -eq 0 ];
+then
+	echo "Docker Exists"
+else
+	#Install docker-ce=18.06.0~ce~3-0~ubuntu
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install -y docker-ce=18.06.0~ce~3-0~ubuntu
+	echo "Docker Installed"
+	#Docker swarm join
+	IFS=$'\n'; arr=( $(docker -H tcp://$SwarmManagerIp:2375 swarm join-token worker) );eval ${arr[1]}
+fi
 
 #Docker Pull ubuntu:16.04 image
 docker pull  $Image
 
-#Docker swarm join
-IFS=$'\n'; arr=( $(docker -H tcp://$SwarmManagerIp:2375 swarm join-token worker) );eval ${arr[1]}
-
 sleep 5s
 #Docker create container
-docker run -itd --restart $ContainerRestartPolicy --name workstation --network $Network --ip $ContainerIp -v $DatasetsVol:/root/Datasets -v $UserHome:/root/Workstation -p $SshPort:22 $Image
+docker run -itd --restart $ContainerRestartPolicy --name $ContainerName --network $Network -v $DatasetsVol:/root/Datasets -v $UserHome:/root/Workstation -p $SshPort:22 $Image
 
 echo "Command to login into the container: ssh -p $SshPort root@$ip"
 echo "Password is root@123"
